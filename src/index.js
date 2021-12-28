d0d9dddfd8566c77d15ebd8ce9d3c0ad47f7aad0
Earth Spacon <rahmatjulmat@gmail.com>
2021-12-28 15:55:43 +0500

    disable strict

--- src/index.js
+++ src/index.js
@@ -4,9 +4,4 @@ import App from './App'
 import 'antd/dist/antd.min.css'
 import './index.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'))
