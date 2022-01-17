1ae8a6e867dd0d506402814dfba3464fae98514d
Earth Spacon <rahmatjulmat@gmail.com>
2022-01-17 17:49:52 +0500

    а

--- src/components/Posts/PostsList.js
+++ src/components/Posts/PostsList.js
@@ -1,16 +1,17 @@
 import { CrudActions } from '../CrudActions'
 
-export const PostsList = ({ filteredPosts, params }) => {
-  return (
+export const PostsList = ({ filteredPosts, params }) =>
+  filteredPosts?.length ? (
+    filteredPosts.map((post) => (
+      <div className='posts' key={post.id}>
+        <h2>{post.title}</h2>
+        <p>{post.body}</p>
+        <CrudActions payload={post} data={filteredPosts} params={params} />
+      </div>
+    ))
+  ) : (
     <>
-      {filteredPosts?.map((post) => (
-        <div className='posts' key={post.id}>
-          <h2>{post.title}</h2>
-          <p>{post.body}</p>
-          <CrudActions payload={post} data={filteredPosts} params={params} />
-        </div>
-      ))}
-      {!filteredPosts?.length ? <p className='no-posts'>NO POSTS!</p> : null}
+      <p className='no-posts'>NO POSTS!</p>
+      <CrudActions data={filteredPosts} params={params} />
     </>
   )
-}
