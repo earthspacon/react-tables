0dab82deafee4fb587c813643a29dfe2ac6fc465
Earth Spacon <rahmatjulmat@gmail.com>
2022-01-17 23:07:48 +0500

    ignore db json

--- src/components/Posts/Pagination.js
+++ src/components/Posts/Pagination.js
@@ -4,8 +4,13 @@ import { useMemo } from 'react'
 export const Pagination = ({ page, totalPages, setPage }) => {
   const pagesArray = useMemo(() => {
     let start = Math.floor((page - 1) / 5) * 5
-    return new Array(5).fill().map((_, idx) => start + idx + 1)
-  }, [page])
+    const pages = new Array(5).fill().map((_, idx) => start + idx + 1)
+    let endPages
+    while (pages <= totalPages) {
+      endPages = pages
+    }
+    return endPages
+  }, [page, totalPages])
 
   const handlePrevious = () => setPage((p) => p - 1)
   const handleNext = () => setPage((p) => p + 1)
